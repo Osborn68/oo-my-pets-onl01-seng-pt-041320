81b92e6d7901e3d3214725dac9fb7d3ed80babe9
Christopher Osborn <z68ozz@gmail.com>
2020-05-16 01:32:44 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
 class Owner
-  # code goes here
+
+attr_reader :name, :species 
+
+@@all = []
+
+def initialize(name)
+  @name = name 
+  @species = "human"
+  @@all << self 
+end
+
+def say_species
+  puts self.species
+end
 end
